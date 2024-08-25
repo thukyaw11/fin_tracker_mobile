c9995804580daf7705f8582dbe9fc19c209cf39b
SoeYuLay <zaya532002@gmail.com>
2024-08-25 20:45:02 +0630

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:expense_tracker_mobile/res/routes.dart';
 import 'package:expense_tracker_mobile/views/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/route_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 5),
+  );
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
